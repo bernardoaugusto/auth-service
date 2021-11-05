7bea5c4d74d54c183a875fcda5db59378c8795b5
Bernardo Augusto Bastos Silva <bernardo.augusto.bastos@gmail.com>
2021-11-05 15:20:30 -0300

    feat: ensure SignUpController returns 400 if no password id provided

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -5,9 +5,19 @@ import { badRequest } from "../helpers/http-helper";
 
 export class SignUpController {
     handle(httpRequest: HttpRequest): HttpResponse {
-        if (!httpRequest.body.name)
-            return badRequest(new MissingParamError("name"));
+        const requiredFields = [
+            "name",
+            "email",
+            "password",
+            "passwordConfirmation",
+        ];
 
-        return badRequest(new MissingParamError("email"));
+        for (const field of requiredFields) {
+            if (!httpRequest.body[field]) {
+                return badRequest(new MissingParamError(field));
+            }
+        }
+
+        return badRequest(new MissingParamError("test"));
     }
 }
