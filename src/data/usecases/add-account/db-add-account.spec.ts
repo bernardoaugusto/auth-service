ae80c54d4bdb8f7f730c316bb03ef067f84aa16e
Bernardo Bastos <bernardo.augusto.bastos@gmail.com>
2023-06-10 15:01:31 -0300

    test: ensure DbAddAcount throws if AddAccountRepository throws

--- src/data/usecases/add-account/db-add-account.spec.ts
+++ src/data/usecases/add-account/db-add-account.spec.ts
@@ -65,7 +65,7 @@ describe("DbAddAccount Usecase", () => {
         expect(encryptSpy).toHaveBeenCalledWith("valid_password");
     });
 
-    it("Should throw id Encrypter throws", async () => {
+    it("Should throw if Encrypter throws", async () => {
         const { sut, encrypterStub } = makeSut();
         jest.spyOn(encrypterStub, "encrypt").mockReturnValueOnce(
             new Promise((resolve, reject) => reject(new Error()))
@@ -98,5 +98,21 @@ describe("DbAddAccount Usecase", () => {
             email: "valid_email@mail.com",
             password: "hashed_password",
         });
+    });
+
+    it("Should throw if AddAccountRepository throws", async () => {
+        const { sut, addAccountRepositoryStub } = makeSut();
+        jest.spyOn(addAccountRepositoryStub, "add").mockReturnValueOnce(
+            new Promise((resolve, reject) => reject(new Error()))
+        );
+
+        const accountData = {
+            name: "valid_name",
+            email: "valid_email@mail.com",
+            password: "valid_password",
+        };
+
+        const promisse = sut.add(accountData);
+        await expect(promisse).rejects.toThrow();
     });
 });
