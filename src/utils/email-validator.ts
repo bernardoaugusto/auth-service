9b2b2c0535b31af53f6b0e12afffe0ac3c833e4d
Bernardo Bastos <bernardo.augusto.bastos@gmail.com>
2023-06-10 13:44:58 -0300

    feat: ensure EmailValidatorAdapter returns false if validator returns true

--- src/utils/email-validator.ts
+++ src/utils/email-validator.ts
@@ -1,8 +1,8 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
+import validator from "validator";
 import { EmailValidator } from "../presentation/protocols/email-validator";
 
 export class EmailValidatorAdapter implements EmailValidator {
     isValid(email: string): boolean {
-        return false;
+        return validator.isEmail(email);
     }
 }
