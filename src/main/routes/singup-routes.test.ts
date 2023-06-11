3cc87f264726736b46f3f3e28a5186b8319cb30b
Bernardo Bastos <bernardo.augusto.bastos@gmail.com>
2023-06-10 22:10:42 -0300

    refactor: connect to mongodb before integration tests

--- src/main/routes/singup-routes.test.ts
+++ src/main/routes/singup-routes.test.ts
@@ -1,8 +1,21 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import request from "supertest";
 import app from "../config/app";
+import { MongoHelper } from "../../infra/db/mongodb/helpers/mongo-helper";
 
 describe("SingUp Routes", () => {
+    beforeAll(async () => {
+        await MongoHelper.connect(process.env.MONGO_URL as string);
+    });
+
+    afterAll(async () => {
+        await MongoHelper.disconnect();
+    });
+
+    beforeEach(async () => {
+        const accountCollection = MongoHelper.getCollection("accounts");
+        await accountCollection.deleteMany({});
+    });
     test("Should return an account on success", async () => {
         await request(app)
             .post("/api/signup")
