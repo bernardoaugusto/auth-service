7d2673492c2cc6a4b285c18be29a1a02260046cb
Bernardo Bastos <bernardo.augusto.bastos@gmail.com>
2023-06-16 15:25:07 -0300

    test: ensure LoginController returns 200 if valid credentials are provided

--- src/presentation/controllers/login/login.spec.ts
+++ src/presentation/controllers/login/login.spec.ts
@@ -1,5 +1,6 @@
 import { LoginController } from "./login";
 import {
+    ok,
     badRequest,
     serverError,
     unauthorized,
@@ -125,5 +126,12 @@ describe("Login Controller", () => {
 
         const httpResponse = await sut.handle(makeFakeRequest());
         expect(httpResponse).toEqual(serverError(new Error()));
+    });
+
+    test("Should return 200 if valid credentials are provided", async () => {
+        const { sut } = makeSut();
+
+        const httpResponse = await sut.handle(makeFakeRequest());
+        expect(httpResponse).toEqual(ok({ accessToken: "any_token" }));
     });
 });
