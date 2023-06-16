8a442ab0721fc4c3e2740d5e199b0e4ffa3a8294
Bernardo Bastos <bernardo.augusto.bastos@gmail.com>
2023-06-16 20:45:29 -0300

    refactor: move Validation to protocols folder

--- src/main/factories/singup/singup-validation.spec.ts
+++ src/main/factories/singup/singup-validation.spec.ts
@@ -1,6 +1,6 @@
 import { ValidationComposite } from "../../../presentation/helpers/validators/validation-composite";
 import { makeSingUpValidation } from "./singup-validation";
-import { Validation } from "../../../presentation/helpers/validators/validation";
+import { Validation } from "../../../presentation/protocols/validation";
 import { RequiredFieldValidation } from "../../../presentation/helpers/validators/required-field-validation";
 import { CompareFieldsValidation } from "../../../presentation/helpers/validators/compare-fields-validation";
 import { EmailValidation } from "../../../presentation/helpers/validators/email-validation";
