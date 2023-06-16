a13646ece30bb66a021e3deebb2d00409373115e
Bernardo Bastos <bernardo.augusto.bastos@gmail.com>
2023-06-16 17:13:02 -0300

    feat: ensure SingUpController calls Validation with correct value

--- src/presentation/controllers/singup/signup.ts
+++ src/presentation/controllers/singup/signup.ts
@@ -4,22 +4,30 @@ import {
     Controller,
     EmailValidator,
     AddAccount,
+    Validation,
 } from "./singup-protocols";
 import { MissingParamError, InvalidParamError } from "../../errors";
 import { badRequest, serverError, ok } from "../../helpers/http-helper";
 
 export class SignUpController implements Controller {
     private readonly emailValidator: EmailValidator;
-
     private readonly addAccount: AddAccount;
+    private readonly validation: Validation;
 
-    constructor(emailValidator: EmailValidator, addAccount: AddAccount) {
+    constructor(
+        emailValidator: EmailValidator,
+        addAccount: AddAccount,
+        validation: Validation
+    ) {
         this.emailValidator = emailValidator;
         this.addAccount = addAccount;
+        this.validation = validation;
     }
 
     async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
         try {
+            this.validation.validate(httpRequest.body);
+
             const requiredFields = [
                 "name",
                 "email",
