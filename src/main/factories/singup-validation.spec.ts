e6ca442401238e435810b00e57964ec22b8bbbc1
Bernardo Bastos <bernardo.augusto.bastos@gmail.com>
2023-06-16 18:00:09 -0300

    refactor: move password comparation to the ValidationComposite

--- src/main/factories/singup-validation.spec.ts
+++ src/main/factories/singup-validation.spec.ts
@@ -1,7 +1,8 @@
 import { ValidationComposite } from "../../presentation/helpers/validators/validation-composite";
 import { makeSingUpValidation } from "./singup-validation";
-import { RequiredFieldValidation } from "../../presentation/helpers/validators/required-field-validation";
 import { Validation } from "../../presentation/helpers/validators/validation";
+import { RequiredFieldValidation } from "../../presentation/helpers/validators/required-field-validation";
+import { CompareFieldsValidation } from "../../presentation/helpers/validators/compare-fields-validation";
 
 jest.mock("../../presentation/helpers/validators/validation-composite");
 
@@ -18,6 +19,10 @@ describe("SingUpValidation Factory", () => {
         ]) {
             validations.push(new RequiredFieldValidation(field));
         }
+
+        validations.push(
+            new CompareFieldsValidation("password", "passwordConfirmation")
+        );
 
         expect(ValidationComposite).toHaveBeenCalledWith(validations);
     });
