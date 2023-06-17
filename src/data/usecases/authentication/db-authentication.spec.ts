87ab72d09b60bced8fa8fefbd9e9c8d91f8d8f62
Bernardo Bastos <bernardo.augusto.bastos@gmail.com>
2023-06-16 21:49:41 -0300

    test: ensure DbAuthentication returns null

--- src/data/usecases/authentication/db-authentication.spec.ts
+++ src/data/usecases/authentication/db-authentication.spec.ts
@@ -62,4 +62,16 @@ describe("DbAuthentication UseCase", () => {
 
         expect(promise).rejects.toThrow();
     });
+
+    it("Should return null if LoadAccountByEmailRepository returns null", async () => {
+        const { sut, loadAccountByEmailRepositoryStub } = makeSut();
+        jest.spyOn(
+            loadAccountByEmailRepositoryStub,
+            "load"
+        ).mockReturnValueOnce(null as any);
+
+        const accessToken = await sut.auth(makeFakeAuthentication());
+
+        expect(accessToken).toBeNull();
+    });
 });
