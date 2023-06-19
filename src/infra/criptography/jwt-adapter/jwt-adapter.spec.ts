829859c73bb3155f578d7ca16640dedc94b9510d
Bernardo Bastos <bernardo.augusto.bastos@gmail.com>
2023-06-19 17:53:15 -0300

    test: ensure JwtAdapter returns a token when sing success

--- src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
+++ src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
@@ -1,6 +1,12 @@
 import jwt from "jsonwebtoken";
 import { JwtAdapter } from "./jwt-adapter";
 
+jest.mock("jsonwebtoken", () => ({
+    async sign(): Promise<string> {
+        return new Promise((resolve) => resolve("any_token"));
+    },
+}));
+
 describe("Jwt Adapter", () => {
     it("Should call sign with correct values", async () => {
         const sut = new JwtAdapter("secret");
@@ -8,5 +14,12 @@ describe("Jwt Adapter", () => {
 
         await sut.encrypt("any_id");
         expect(signSpy).toHaveBeenCalledWith({ id: "any_id" }, "secret");
+    });
+
+    it("Should return a token on sing success", async () => {
+        const sut = new JwtAdapter("secret");
+
+        const accessToken = await sut.encrypt("any_id");
+        expect(accessToken).toBe("any_token");
     });
 });
