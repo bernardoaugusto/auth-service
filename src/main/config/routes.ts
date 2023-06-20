051602d42775930bd5c095eccd5906b7c8ce7ab6
Bernardo Bastos <bernardo.augusto.bastos@gmail.com>
2023-06-19 21:59:36 -0300

    refactor: change routes to use filesystem

--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -1,13 +1,15 @@
 import { Express, Router } from "express";
-import fg from "fast-glob";
 import env from "./env";
+import { readdirSync } from "fs";
 
 export default (app: Express): void => {
     const extention = env.nodeEnv === "production" ? "js" : "ts";
     const router = Router();
     app.use("/api", router);
 
-    fg.sync(`**/main/routes/**routes.${extention}`).forEach(async (file) =>
-        (await import(`../../../${file}`)).default(router)
-    );
+    readdirSync(`${__dirname}/../routes`).map(async (file) => {
+        if (!file.includes(".test.")) {
+            (await import(`../routes/${file}`)).default(router);
+        }
+    });
 };
