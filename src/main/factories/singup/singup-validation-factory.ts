23e1413a0405b6855f75848f372b6e8bfb01b0c5
Bernardo Bastos <bernardo.augusto.bastos@gmail.com>
2023-06-19 21:02:17 -0300

    refactor: move EmailValidator Adapter to the main layer

--- src/main/factories/singup/singup-validation-factory.ts
+++ src/main/factories/singup/singup-validation-factory.ts
@@ -5,7 +5,7 @@ import {
     RequiredFieldValidation,
 } from "../../../presentation/helpers/validators";
 import { Validation } from "../../../presentation/protocols/validation";
-import { EmailValidatorAdapter } from "../../../utils/email-validator-adapter";
+import { EmailValidatorAdapter } from "../../adapters/validators/email-validator-adapter";
 
 export const makeSingUpValidation = (): ValidationComposite => {
     const validations: Validation[] = [];
