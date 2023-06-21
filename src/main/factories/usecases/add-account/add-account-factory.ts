db88546babcfbc524c42db166e58611d6ccba657
Bernardo Bastos <bernardo.augusto.bastos@gmail.com>
2023-06-21 15:33:19 -0300

    feat: add LoadAccountByEmailRepository in makeDbAddAccount

--- src/main/factories/usecases/add-account/add-account-factory.ts
+++ src/main/factories/usecases/add-account/add-account-factory.ts
@@ -7,5 +7,9 @@ export const makeDbAddAccount = (): AddAccount => {
     const salt = 12;
     const bcryptAdapter = new BcryptAdapter(salt);
     const addAccountRepository = new AccountMongoRepository();
-    return new DbAddAccount(bcryptAdapter, addAccountRepository);
+    return new DbAddAccount(
+        bcryptAdapter,
+        addAccountRepository,
+        addAccountRepository
+    );
 };
