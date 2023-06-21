e68ea4147f174c7bbac5f37a797fb7fa7b29e7f8
Bernardo Bastos <bernardo.augusto.bastos@gmail.com>
2023-06-21 13:34:02 -0300

    feat: add dbAuthentication in makeSignUpController

--- src/main/factories/singup/singup.ts
+++ src/main/factories/singup/singup.ts
@@ -1,3 +1,4 @@
+import env from "../../config/env";
 import { DbAddAccount } from "../../../data/usecases/add-account/db-add-account";
 import { BcryptAdapter } from "../../../infra/criptography/bcrypt-adapter/bcrypt-adapter";
 import { AccountMongoRepository } from "../../../infra/db/mongodb/account/account-mongo-repository";
@@ -6,16 +7,27 @@ import { Controller } from "../../../presentation/protocols";
 import { LogControllerDecorator } from "../../decorators/log-controller-decorator";
 import { LogMongoRepository } from "../../../infra/db/mongodb/log/log-mongo-repository";
 import { makeSingUpValidation } from "./singup-validation-factory";
+import { JwtAdapter } from "../../../infra/criptography/jwt-adapter/jwt-adapter";
+import { DbAuthentication } from "../../../data/usecases/authentication/db-authentication";
 
 export const makeSignUpController = (): Controller => {
     const salt = 12;
-    const encryper = new BcryptAdapter(salt);
+    const bcryptAdapter = new BcryptAdapter(salt);
     const addAccountRepository = new AccountMongoRepository();
-    const addAccount = new DbAddAccount(encryper, addAccountRepository);
+    const addAccount = new DbAddAccount(bcryptAdapter, addAccountRepository);
+    const jwtAdapter = new JwtAdapter(env.jwtSecret);
+
+    const dbAuthentication = new DbAuthentication(
+        addAccountRepository,
+        bcryptAdapter,
+        jwtAdapter,
+        addAccountRepository
+    );
 
     const signUpController = new SignUpController(
         addAccount,
-        makeSingUpValidation()
+        makeSingUpValidation(),
+        dbAuthentication
     );
 
     const logMongoRepository = new LogMongoRepository();
