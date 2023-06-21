cc3257a2ad4fb523cad3eca565520c02b6de0726
Bernardo Bastos <bernardo.augusto.bastos@gmail.com>
2023-06-21 13:29:53 -0300

    feat: ensure SingUpController calls Authentication with correct values

--- src/presentation/controllers/singup/signup-controller.ts
+++ src/presentation/controllers/singup/signup-controller.ts
@@ -4,13 +4,15 @@ import {
     Controller,
     AddAccount,
     Validation,
+    Authentication,
 } from "./singup-controller-protocols";
 import { badRequest, serverError, ok } from "../../helpers/http/http-helper";
 
 export class SignUpController implements Controller {
     constructor(
         private readonly addAccount: AddAccount,
-        private readonly validation: Validation
+        private readonly validation: Validation,
+        private readonly authentication: Authentication
     ) {}
 
     async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
@@ -28,6 +30,8 @@ export class SignUpController implements Controller {
                 email,
                 password,
             });
+
+            await this.authentication.auth({ email, password });
 
             return ok(account);
         } catch (error) {
