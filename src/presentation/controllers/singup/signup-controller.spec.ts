ebbe0d2e0d3933fa22aeafd9367c3b9bf6bc9c55
Bernardo Bastos <bernardo.augusto.bastos@gmail.com>
2023-06-21 13:35:24 -0300

    test: ensure SingUpController returns 500 if Authentication throws

--- src/presentation/controllers/singup/signup-controller.spec.ts
+++ src/presentation/controllers/singup/signup-controller.spec.ts
@@ -141,4 +141,14 @@ describe("SingUP Controller", () => {
             password: "any_password",
         });
     });
+
+    it("Should return 500 if Authentication throws", async () => {
+        const { sut, authenticationStub } = makeSut();
+        jest.spyOn(authenticationStub, "auth").mockResolvedValueOnce(
+            new Promise((resolve, reject) => reject(new Error()))
+        );
+
+        const httpResponse = await sut.handle(makeFakeRequest());
+        expect(httpResponse).toEqual(serverError(new Error()));
+    });
 });
